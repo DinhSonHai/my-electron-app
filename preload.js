e53453f38353399419089ba8f0d2552e2998db7f
DinhSonHai <sdinhhai@gmail.com>
2025-07-21 22:19:43 +0700

    feat: dark mode

--- preload.js
+++ preload.js
@@ -4,6 +4,10 @@ contextBridge.exposeInMainWorld('versions', {
   node: () => process.versions.node,
   chrome: () => process.versions.chrome,
   electron: () => process.versions.electron,
-  ping: () => ipcRenderer.invoke('ping')
-  // we can also expose variables, not just functions
+  ping: () => ipcRenderer.invoke('ping'),
+})
+
+contextBridge.exposeInMainWorld('darkMode', {
+  toggle: () => ipcRenderer.invoke('dark-mode:toggle'),
+  system: () => ipcRenderer.invoke('dark-mode:system')
 })
